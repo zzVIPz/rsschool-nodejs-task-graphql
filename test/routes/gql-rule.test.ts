dc1cb448b594930ba26085b9a236f1ef49e2e6a8
Bogdan <noskovbogdan@gmail.com>
2023-07-16 22:11:54 +0300

    feat: update

--- test/routes/gql-rule.test.ts
+++ test/routes/gql-rule.test.ts
@@ -7,18 +7,6 @@ await test('gql-rule', async (t) => {
 
   await t.test('Trigger depth limit rule.', async (t) => {
     const { body: user1 } = await createUser(app);
-    const { body: user2 } = await createUser(app);
-    const { body: user3 } = await createUser(app);
-    const { body: user4 } = await createUser(app);
-    const { body: user5 } = await createUser(app);
-    const { body: user6 } = await createUser(app);
-    const { body: user7 } = await createUser(app);
-
-    await subscribeTo(app, user1.id, user2.id);
-    await subscribeTo(app, user2.id, user3.id);
-    await subscribeTo(app, user3.id, user4.id);
-    await subscribeTo(app, user5.id, user6.id);
-    await subscribeTo(app, user6.id, user7.id);
 
     const {
       body: { errors },
